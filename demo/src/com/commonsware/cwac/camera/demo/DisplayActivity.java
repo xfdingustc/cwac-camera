ffecc7739b14ac4dbc4f7a74eca5295ef7cb5afe
Mark Murphy <mmurphy@commonsware.com>
2013-08-31 19:22:03 -0400

    more reliable DisplayActivity decode

--- demo/src/com/commonsware/cwac/camera/demo/DisplayActivity.java
+++ demo/src/com/commonsware/cwac/camera/demo/DisplayActivity.java
@@ -12,19 +12,26 @@ public class DisplayActivity extends Activity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    
+
     if (imageToShow == null) {
       Toast.makeText(this, R.string.no_image, Toast.LENGTH_LONG).show();
       finish();
     }
     else {
       ImageView iv=new ImageView(this);
+      BitmapFactory.Options opts=new BitmapFactory.Options();
+
+      opts.inPurgeable=true;
+      opts.inInputShareable=true;
+      opts.inMutable=false;
+      opts.inSampleSize=2;
 
       iv.setImageBitmap(BitmapFactory.decodeByteArray(imageToShow,
                                                       0,
-                                                      imageToShow.length));
+                                                      imageToShow.length,
+                                                      opts));
       imageToShow=null;
-      
+
       iv.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
       setContentView(iv);
     }
