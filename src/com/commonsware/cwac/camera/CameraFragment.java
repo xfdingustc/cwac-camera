e443b72e85855dcc5c8fcbdde3ec76aaa5b8a76d
Mark Murphy <mmurphy@commonsware.com>
2013-11-24 14:55:02 -0500

    code comments

--- src/com/commonsware/cwac/camera/CameraFragment.java
+++ src/com/commonsware/cwac/camera/CameraFragment.java
@@ -22,11 +22,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+/**
+ * Primary class for using `CameraView` as a fragment. Just
+ * add this as a fragment, no different than any other
+ * fragment that you might use, and it will handle the
+ * camera preview, plus give you controls to take pictures,
+ * etc.
+ */
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class CameraFragment extends Fragment {
   private CameraView cameraView=null;
   private CameraHost host=null;
 
+  /*
+   * (non-Javadoc)
+   * 
+   * @see android.app.Fragment#onCreateView(android.view.
+   * LayoutInflater, android.view.ViewGroup,
+   * android.os.Bundle)
+   */
   @Override
   public View onCreateView(LayoutInflater inflater,
                            ViewGroup container,
@@ -55,6 +69,11 @@ public class CameraFragment extends Fragment {
     this.cameraView=cameraView;
   }
 
+  /**
+   * @return the CameraHost instance you want to use for
+   *         this fragment, where the default is an instance
+   *         of the stock SimpleCameraHost.
+   */
   public CameraHost getHost() {
     if (host == null) {
       host=new SimpleCameraHost(getActivity());
@@ -75,6 +94,10 @@ public class CameraFragment extends Fragment {
     cameraView.takePicture(needBitmap, needByteArray);
   }
 
+  /**
+   * @return true if we are recording video right now, false
+   *         otherwise
+   */
   public boolean isRecording() {
     return(cameraView == null ? false : cameraView.isRecording());
   }
@@ -87,22 +110,48 @@ public class CameraFragment extends Fragment {
     cameraView.stopRecording();
   }
 
+  /**
+   * @return the orientation of the screen, in degrees
+   *         (0-360)
+   */
   public int getDisplayOrientation() {
     return(cameraView.getDisplayOrientation());
   }
 
+  /**
+   * Call this to lock the camera to landscape mode (with a
+   * parameter of true), regardless of what the actual
+   * screen orientation is.
+   * 
+   * @param enable
+   *          true to lock the camera to landscape, false to
+   *          allow normal rotation
+   */
   public void lockToLandscape(boolean enable) {
     cameraView.lockToLandscape(enable);
   }
 
+  /**
+   * Call this to begin an auto-focus operation (e.g., in
+   * response to the user tapping something to focus the
+   * camera).
+   */
   public void autoFocus() {
     cameraView.autoFocus();
   }
 
+  /**
+   * Call this to cancel an auto-focus operation that had
+   * been started via a call to autoFocus().
+   */
   public void cancelAutoFocus() {
     cameraView.cancelAutoFocus();
   }
 
+  /**
+   * @return true if auto-focus is an option on this device,
+   *         false otherwise
+   */
   public boolean isAutoFocusAvailable() {
     return(cameraView.isAutoFocusAvailable());
   }
@@ -111,10 +160,14 @@ public class CameraFragment extends Fragment {
     cameraView.restartPreview();
   }
 
+  /**
+   * @return the name of the current flash mode, as reported
+   *         by Camera.Parameters
+   */
   public String getFlashMode() {
     return(cameraView.getFlashMode());
   }
-  
+
   public ZoomTransaction zoomTo(int level) {
     return(cameraView.zoomTo(level));
   }
